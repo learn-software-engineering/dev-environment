e3027303163d8fcdaaddf40d0e6132697a5e27ec
learn-software-engineering <learn.software.eng@gmail.com>
2022-10-27 23:55:58 +0000

    Publish revision 643c7299b28e3f42323e91246f13d18785324ca1

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "/dev-environment/blog/post-modified/",
         "teaser": null
       },{
-        "title": "Post: Standard wit long title to test overlay image",
+        "title": "Post: Standard with long title to test overlay image",
         "excerpt":"First paragraph to test.     This post has a manual excerpt &lt;!--more--&gt; set after the second paragraph. The following YAML Front Matter has also be applied:   excerpt_separator: \"&lt;!--more--&gt;\"   Section 1   Second paragraph.   Section 1.1   Third paragraph.   Section 1.1.1   Fourth paragraph.   Section 1.1.2   Fifth paragraph.   Section 1.2   Sixth paragraph.   Section 2   Seventh paragraph.   Section 2.1   Eighth paragraph.   Section 2.2   Ninth paragraph.   Section 2.2.1   Tenth paragraph.   Section 2.2.2   Eleventh paragraph.  ","categories": ["Blog"],
         "tags": ["Post Formats","readability","standard"],
         "url": "/dev-environment/blog/post-standard/",
