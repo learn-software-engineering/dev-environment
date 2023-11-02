2696acd16c927e5c6c312767e2731e2f0adce1d8
learn-software-engineering <learn.software.eng@gmail.com>
2023-11-02 10:43:42 +0000

    Publish revision 67324f9f0f1a1034652c35da264cdecdcc4dcd99

--- docs/js/search.js
+++ docs/js/search.js
@@ -1733,7 +1733,7 @@
     }
   });
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/dropdown.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/dropdown.ts
   (() => {
     const hide = (dropdown) => {
       dropdown.classList.remove("show");
@@ -1803,7 +1803,7 @@
     });
   })();
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/keyboard.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/keyboard.ts
   var Keyboard = class {
     keys = {};
     events = [];
@@ -1849,7 +1849,7 @@
     });
   })();
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/navigator.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/navigator.ts
   var Navigator = class {
     modal() {
       return document.querySelector(".search-modal-container.active");
@@ -1940,11 +1940,11 @@
     }
   };
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/i18n.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/i18n.ts
   var i18n = new Translator(params_default.i18n, params_default.defaultLang);
   var i18n_default = i18n;
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/engine.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/engine.ts
   var import_fuse = __toESM(require_fuse());
   var Engine = class {
     index;
@@ -2119,7 +2119,7 @@
   var engine = new Engine();
   var engine_default = engine;
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/form.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/form.ts
   var Form = class {
     constructor(spinner, renderer) {
       this.spinner = spinner;
@@ -2381,7 +2381,7 @@
     }
   };
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/spinner.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/spinner.ts
   var Spinner = class {
     constructor(ele) {
       this.ele = ele;
@@ -2411,7 +2411,7 @@
     }
   };
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/renderer.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/renderer.ts
   var Renderer = class {
     constructor(container, statistics, spinner) {
       this.container = container;
@@ -2670,7 +2670,7 @@
     }
   };
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/shortcuts.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/shortcuts.ts
   var Navigate = {
     kbds: ["\u2191", "\u2193"],
     action: i18n_default.translate("to_navigate")
@@ -2708,7 +2708,7 @@ ${this.renderKbds(shortcut.kbds)}
     }
   };
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/modal.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/modal.ts
   var searchShortcut = {
     kbds: [params_default.shortcut_search],
     action: i18n_default.translate("to_search")
@@ -2804,7 +2804,7 @@ ${this.renderKbds(shortcut.kbds)}
     });
   })();
 
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.5.0/assets/search/js/search.ts
+  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/search@v0.6.0/assets/search/js/search.ts
   var Search = class {
     container;
     form;
