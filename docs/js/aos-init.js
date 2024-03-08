e7ffc7b08e95ea38a477a0e2085bfaff8fd46066
learn-software-engineering <learn.software.eng@gmail.com>
2024-03-08 13:01:35 +0000

    Publish revision 93d76b93ee0a33964827a47e44cd22c509e75005

--- docs/js/aos-init.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  // ns-hugo:/tmp/hugo_cache/modules/filecache/modules/pkg/mod/github.com/hugomods/aos@v0.1.0/assets/mods/aos/js/options.ts
-  var options_default = {};
-
-  // <stdin>
-  AOS.init(options_default);
-})();
